d3881cadfaaf61c8db77c419105b64f24765c51d
Plgdhd <aivanuk161@gmail.com>
2025-05-20 22:31:45 +0300

    plgdhd

--- frontend/scripts/loginCheck.js
+++ frontend/scripts/loginCheck.js
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="logo">Belines</div>
                 <div class="nav-links">
                     <a href="../index.html">Главная</a>
-                    <a href="./html/flights.html">Рейсы</a>
-                    <a href="./html/about.html">О нас</a>
-                    <a href="./html/profile.html"> Профиль </a>
+                    <a href="../html/flights.html">Рейсы</a>
+                    <a href="../html/about.html">О нас</a>
+                    <a href="../html/profile.html"> Профиль </a>
                 </div>
                 <div class="auth-buttons">
                     <button id="exit-btn" class="./login-btn" onclick="window.location.href='./html/login.html'">Выйти</button>
