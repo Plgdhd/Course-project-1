1056c3945bddcfa31e1243e0eefc4b9b41e327fd
Plgdhd <aivanuk161@gmail.com>
2025-05-20 20:19:02 +0300

    node navigation

--- frontend/server.js
+++ frontend/server.js
@@ -4,12 +4,40 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/index.html'));
+});
+
+app.get('/flights', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/flights.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/about.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/register.html'));
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/profile.html'));
+});
+
+app.get('/flight-card', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/flight-card.html'));
+});
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/html/index.html'));
+    res.redirect('/');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
